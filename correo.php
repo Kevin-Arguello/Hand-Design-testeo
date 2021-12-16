ee96bbca2de29683b8fde94a704f3050a605fec0
Kevin-Arguello <86067926+Kevin-Arguello@users.noreply.github.com>
2021-12-16 17:26:34 -0300

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,17 @@
+<?php
+
+    $destinatario = 'kev.creativedesign@gmail.com';
+
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+
+    $header = "Enviado desde la pagina KeV";
+    $mensajeCompleto = $mensaje . "\nAtentamente:" . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script>alert('correo enviado exitosamente)</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\", 1000) </script>";
+
+?>
